035773f7e7dadd8fc0d9fde145e7dec1228fa99c
Siddharth Negi <siddharthnegi35@gmail.com>
2026-01-06 22:57:36 +0530

    Fix: Add chart components and lucide-react

--- components/charts/BarChartComponent.tsx
+++ components/charts/BarChartComponent.tsx
@@ -1,15 +1,15 @@
 'use client';
-'use client';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 export default function BarChartComponent({ data }: { data: any[] }) {
   return (
-    <ResponsiveContainer width="100%" height={350}>
+    <ResponsiveContainer width='100%' height={350}>
       <BarChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='name' />
         <YAxis />
         <Tooltip />
-        <Bar dataKey="value" fill="#82ca9d" />
+        <Bar dataKey='value' fill='#82ca9d' />
       </BarChart>
     </ResponsiveContainer>
   );
--- components/charts/LineChartComponent.tsx
+++ components/charts/LineChartComponent.tsx
@@ -1,15 +1,15 @@
 'use client';
-'use client';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 export default function LineChartComponent({ data }: { data: any[] }) {
   return (
-    <ResponsiveContainer width="100%" height={350}>
+    <ResponsiveContainer width='100%' height={350}>
       <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='name' />
         <YAxis />
         <Tooltip />
-        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+        <Line type='monotone' dataKey='value' stroke='#8884d8' />
       </LineChart>
     </ResponsiveContainer>
   );
